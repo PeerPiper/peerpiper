8d17a603b47866ed862b61ce6cc5a9e952ebbd92
Doug Anderson444 <douganderson444@gmail.com>
2023-12-15 21:56:11 -0400

    use new API

--- crates/peerpiper-native/src/lib.rs
+++ crates/peerpiper-native/src/lib.rs
@@ -7,7 +7,7 @@ use std::net::Ipv4Addr;
 
 use peerpiper_core::{
     error::Error,
-    events::{Network, NetworkEvent},
+    events::NetworkEvent,
     libp2p::{api, behaviour, swarm},
 };
 
@@ -17,7 +17,7 @@ pub async fn start(mut tx: mpsc::Sender<NetworkEvent>) -> Result<(), Box<dyn std
     let peer_id = *swarm.local_peer_id();
     tracing::info!("Local peer id: {:?}", peer_id);
 
-    let (mut network_client, mut network_events, network_event_loop) = api::new(swarm).await?;
+    let (mut network_client, mut network_events, network_event_loop) = api::new(swarm).await;
 
     // We need to start the network event loop first in order to listen for our address
     let network_handle = tokio::spawn(async move { network_event_loop.run().await });
@@ -67,8 +67,8 @@ pub async fn start(mut tx: mpsc::Sender<NetworkEvent>) -> Result<(), Box<dyn std
         loop {
             let event = network_events.select_next_some().await;
             tracing::debug!("Network event: {:?}", event);
-            if let Err(swarm_event) = tx.send(event).await {
-                tracing::error!("Failed to send swarm event: {:?}", swarm_event);
+            if let Err(network_event) = tx.send(event).await {
+                tracing::error!("Failed to send swarm event: {:?}", network_event);
                 break;
             }
         }
