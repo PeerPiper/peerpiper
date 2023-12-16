1ea71d14578a540424432e5d9aa7f6897f684d85
Doug Anderson444 <douganderson444@gmail.com>
2023-12-16 07:40:56 -0400

    progress

--- examples/svelte-app/src/lib/examples/Basic.svelte
+++ examples/svelte-app/src/lib/examples/Basic.svelte
@@ -2,23 +2,28 @@
 	// A basic call to the peerpiper wasm-bindgen API to ping and show pings.
 	import peerpiper from '../../../../../crates/peerpiper-browser/Cargo.toml';
 
+	let exportsString = 'Loading...';
+
 	async function loadWasm() {
 		if (!import.meta.env.SSR) {
 			// This code will only run in the browser
-			const exports = await peerpiper();
+			const mod = await peerpiper();
 
 			// Use functions which were exported from Rust...
-			console.log({ exports });
+			console.log({ mod });
+
+			// convert exports to string:
+			exportsString = mod.connect.toString();
 
 			// Start peerpiper-server to get a local Multiaddr at port 8080:
 			const res = await fetch('http://localhost:8080/');
 			const addr = await res.text();
 
 			let onping = (ping) => {
-				console.log({ ping });
+				console.log('Event emitted: ', { ping });
 			};
 
-			exports.connect(addr);
+			mod.connect(addr, onping);
 		}
 	}
 
@@ -26,3 +31,5 @@
 </script>
 
 Ping PeerPiper
+<br />
+{exportsString}
