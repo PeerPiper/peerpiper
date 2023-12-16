3e62ed8c7378eecb7cd25b7ef8ced23eaf958f5e
Doug Anderson444 <douganderson444@gmail.com>
2023-12-15 21:54:27 -0400

    switch to API model

--- crates/peerpiper-browser/src/wasm.rs
+++ crates/peerpiper-browser/src/wasm.rs
@@ -1,46 +1,45 @@
-// #![cfg(target_arch = "wasm32")]
-
 use peerpiper_core::error::Error;
+use peerpiper_core::events::NetworkEvent;
+use peerpiper_core::libp2p::api;
 use peerpiper_core::libp2p::{
-    behaviour::{self, BehaviourEvent},
+    behaviour::{self},
     swarm,
 };
 
 use futures::StreamExt;
 use libp2p::core::Multiaddr;
-use libp2p::ping;
-use libp2p::swarm::SwarmEvent;
+use wasm_bindgen_futures::spawn_local;
 
 pub async fn start(libp2p_endpoint: String) -> Result<(), Error> {
     tracing::info!("Spawning swarm. Using multiaddr {:?}", libp2p_endpoint);
 
-    let mut swarm = swarm::create(behaviour::build).map_err(Error::CreateSwarm)?;
+    let swarm = swarm::create(behaviour::build).map_err(Error::CreateSwarm)?;
 
-    let addr = libp2p_endpoint.parse::<Multiaddr>()?;
-    tracing::info!("Dialing {addr}");
-    swarm.dial(addr)?;
+    let _peer_id = *swarm.local_peer_id();
 
-    loop {
-        futures::select! {
-            event = swarm.next() => {
-                match event.unwrap() {
-                    SwarmEvent::Behaviour(BehaviourEvent::Ping(ping::Event { result: Err(e), .. })) => {
-                        tracing::error!("Ping failed: {:?}", e);
+    let (mut network_client, mut network_events, network_event_loop) = api::new(swarm).await;
 
-                        break;
-                    }
-                    SwarmEvent::Behaviour(BehaviourEvent::Ping(ping::Event {
-                        peer,
-                        result: Ok(rtt),
-                        ..
-                    })) => {
-                        tracing::info!("Ping successful: RTT: {rtt:?}, from {peer}");
-                    }
-                    evt => tracing::info!("Swarm event: {:?}", evt),
-                }
-            }
+    spawn_local(network_event_loop.run());
+
+    let remote_address = libp2p_endpoint.parse::<Multiaddr>()?;
+
+    match network_client.dial(remote_address.clone()).await {
+        Ok(_) => {
+            println!("☎️ 🎉 Dialed remote peer at {}", remote_address);
+        }
+        Err(err) => {
+            eprintln!("Failed to dial remote peer at {}: {}", remote_address, err);
         }
     }
 
-    Ok(())
+    loop {
+        let event = network_events.select_next_some().await;
+        match event {
+            NetworkEvent::Ping { peer, rtt } => {
+                tracing::info!("Ping successful: RTT: {rtt:?}, from {peer}");
+            }
+            evt => tracing::info!("Swarm event: {:?}", evt),
+        }
+        // tracing::debug!("Network event: {:?}", event);
+    }
 }
