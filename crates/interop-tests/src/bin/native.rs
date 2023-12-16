d4c302c5988fc6c44ded51e898311e05d4389157
Doug Anderson444 <douganderson444@gmail.com>
2023-12-15 20:38:53 -0400

    add core debug

--- crates/interop-tests/src/bin/native.rs
+++ crates/interop-tests/src/bin/native.rs
@@ -26,7 +26,7 @@ const MAX_CHANNELS: usize = 16;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _ = tracing_subscriber::fmt()
         .with_env_filter(
-            "interop_tests_native=debug,peerpiper_native=debug,libp2p_webrtc=info,libp2p_ping=debug",
+            "interop_tests_native=debug,peerpiper_native=debug,peerpiper_core=debug,libp2p_webrtc=info,libp2p_ping=debug",
         )
         .try_init();
 
