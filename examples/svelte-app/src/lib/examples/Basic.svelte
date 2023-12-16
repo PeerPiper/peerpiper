b501042212e0af2550b21d83a64122a0643da6cc
Doug Anderson444 <douganderson444@gmail.com>
2023-12-16 16:58:52 -0400

    stringify wasm code in Basic example

--- examples/svelte-app/src/lib/examples/Basic.svelte
+++ examples/svelte-app/src/lib/examples/Basic.svelte
@@ -2,6 +2,9 @@
 	// A basic call to the peerpiper wasm-bindgen API to ping and show pings.
 	import peerpiper from '../../../../../crates/peerpiper-browser/Cargo.toml';
 
+	import peerpiper_browser_js from '../../../../../crates/peerpiper-browser/pkg/peerpiper_browser.js?raw';
+	import wasmURL from '../../../../../crates/peerpiper-browser/pkg/peerpiper_browser_bg.wasm?url';
+
 	let exportsString = 'Loading...';
 
 	async function loadWasm() {
@@ -12,9 +15,6 @@
 			// Use functions which were exported from Rust...
 			console.log({ mod });
 
-			// convert exports to string:
-			exportsString = mod.connect.toString();
-
 			// Start peerpiper-server to get a local Multiaddr at port 8080:
 			const res = await fetch('http://localhost:8080/');
 			const addr = await res.text();
@@ -24,6 +24,20 @@
 			};
 
 			mod.connect(addr, onping);
+
+			// let importables = [{ 'component:cargo-comp/peerpiper-browser': peerpiper_browser_js },
+			// { 'peerpiper_browser_bg.wasm': peerpiper_browser_bg_wasm}];
+			// Load the wasm component bytes as an array buffer
+			let wasmBytes = await fetch(wasmURL).then((res) => res.arrayBuffer());
+
+			// make a blob url of the bytes of the wasm file
+			let wasmBlobUrl = URL.createObjectURL(new Blob([wasmBytes], { type: 'application/wasm' }));
+			// find and replace
+			// code = code.replace(`new URL('./${fileName}', import.meta.url)`, `'${wasmBlobUrl}'`);
+			exportsString = peerpiper_browser_js.replace(
+				`new URL('peerpiper_browser_bg.wasm', import.meta.url)`,
+				`'${wasmBlobUrl}'`
+			);
 		}
 	}
 
