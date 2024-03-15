330ce73164da33b68b6826f6af7b538f0fd31068
Doug Anderson444 <douganderson444@gmail.com>
2024-03-15 12:52:58 -0300

    add graph to Loader.svelte

--- examples/svelte-app/src/lib/examples/Loader.svelte
+++ examples/svelte-app/src/lib/examples/Loader.svelte
@@ -1,7 +1,8 @@
 <script>
 	// a component to send data to a plugin
-	import { onMount, createEventDispatcher } from 'svelte';
+	import { onMount, tick, createEventDispatcher } from 'svelte';
 	import peerpiper from '../../../../../crates/peerpiper-browser/Cargo.toml';
+	import { Wurbo, wurboIn } from 'wurbo';
 
 	export let pluginFile;
 
@@ -11,9 +12,27 @@
 	let piper;
 
 	const dispatch = createEventDispatcher();
+	/**
+	 * The rendered component as a string of HTML
+	 * @type {string | null}
+	 */
+	let renderedHTML;
+	/**
+	 * The module that loads the WebAssembly component
+	 *
+	 * @type
+	 */
+	let wurbo;
+
+	let Graph = null;
 
 	onMount(async () => {
 		piper = await peerpiper();
+
+		// init the graph utility
+		const g = await import('./graph/graph.js');
+		console.log('g', g);
+		g.$init.then(() => (Graph = new g.provider.Graph()));
 	});
 
 	// this fires when todos change; let's emit an event to update any listeners consuming this component
@@ -28,6 +47,8 @@
 				// TODO: Figure out why the Errors don't propagate back up here. It gets stuck in wasm-bindgen
 				let res = await piper.command(command);
 				console.log('command result', res);
+				load(res);
+
 				pluginFile = null; // reset loader state
 				fileinput.value = null; // reset file input
 			} catch (error) {
@@ -58,6 +79,66 @@
 		reader.readAsArrayBuffer(plugin);
 		// reader.readAsText(plugin);
 	};
+
+	// OnFileLoaded: Once the wasm bytes are loaded, we can setup the plugin
+	// key is a cid string
+	async function load(cid) {
+		// get bytes from piper command using Get
+		let command = `{ "System": { "Get": { "key": "${cid}" } } }`;
+		let res = await piper.command(command);
+
+		// setup the plugin
+		// turn the result bytes into an array buffer
+		let bytes = new Uint8Array(res);
+		let arrayBuffer = bytes.buffer;
+
+		// use Graph to get the importables from the bytes
+		let imports;
+		try {
+			let component = Graph.addComponent('loaded', bytes);
+			console.log('component', component);
+			imports = component.imports;
+		} catch (error) {
+			console.error('error getting importables', { error });
+		}
+
+		// filter to select any that contains `/wurbo-in`, set the importable to the `wurboIn` function,
+		let importables = imports
+			.filter((i) => i.name.includes('/wurbo-in'))
+			.map((i) => {
+				// trim string after the @
+				let name = i.name.split('@')[0];
+				return { [name]: wurboIn };
+			});
+
+		console.log({ importables });
+
+		// load the import handles into the Wasm component and get the ES module returned
+		wurbo = new Wurbo({ arrayBuffer, importables }, async (payload) => {
+			// Relay emitted commands from the Wasm component to PiperNet
+			console.log('Command emitted: ', { payload });
+			try {
+				return await piper.command(payload);
+			} catch (error) {
+				// it's ok to fail silently, not all messages are commands
+			}
+		});
+
+		// call `render` with your inputs for the component
+		renderedHTML = await wurbo.render({
+			tag: 'all-content',
+			val: {}
+		});
+	}
+
+	// Once the HTML is rendered and the module is loaded, we can activate the event emitters
+	$: if (renderedHTML && wurbo)
+		(async () => {
+			// wait for the DOM to reflect the updates first
+			await tick();
+			// once the DOM has our elements loaded, we can activate the aggregated event emitters
+			wurbo.activate();
+		})();
 </script>
 
 <input
@@ -68,7 +149,7 @@
 	bind:this={fileinput}
 />
 <div
-	class="flex justify-center cursor-pointer border border-green-400 rounded-md px-4 py-2 my-1"
+	class="flex justify-center cursor-pointer border border-green-400 rounded-md px-4 py-2 my-1 shadow"
 	on:keypress={() => {
 		fileinput.click();
 	}}
@@ -76,7 +157,14 @@
 		fileinput.click();
 	}}
 >
-	Load *.wasm file
+	<div class="flex">
+		Load wurbo *.wasm file (must export <span class="font-mono mx-1 px-1 bg-amber-100 rounded-lg"
+			>wurbo-in</span
+		>
+		and import
+		<span class="font-mono mx-1 px-1 bg-amber-100 rounded-lg">wurbo-out</span>
+		)
+	</div>
 </div>
 
 <!-- Display bytes -->
@@ -86,3 +174,9 @@
 		<div class="flex-1 flex-row">{@html pluginFile}</div>
 	</div>
 {/if}
+
+<div>
+	{#if renderedHTML}
+		{@html renderedHTML}
+	{/if}
+</div>
