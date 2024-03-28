540b8c6a1d09304a2e6a0a2dc773708795bf268a
Doug Anderson444 <douganderson444@gmail.com>
2024-03-28 08:08:25 -0300

    bump deps

--- examples/svelte-app/vite.config.js
+++ examples/svelte-app/vite.config.js
@@ -1,10 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
 import rust from '@wasm-tool/rollup-plugin-rust';
 
 export default defineConfig({
 	plugins: [rust(), sveltekit()],
-	server: { fs: { strict: false } },
+	server: {
+		fs: {
+			strict: false,
+			allow: [
+				// search up for workspace root
+				searchForWorkspaceRoot(process.cwd())
+			]
+		}
+	},
 	build: {
 		minify: false
 	},
