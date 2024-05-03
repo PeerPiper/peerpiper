e6fb0f1460f57abb288f2ec2d1e2e2f9a22792da
Doug Anderson444 <douganderson444@gmail.com>
2024-05-02 21:55:31 -0400

    use built css from submodules

--- packages/integrity-wallet/inner-app/src/main.js
+++ packages/integrity-wallet/inner-app/src/main.js
@@ -1,8 +1,9 @@
-import './app.css'
-import App from './App.svelte'
+import './app.css';
+import '../../../../submodules/seed-keeper/crates/seed-keeper-wit-ui/output.css';
+import App from './App.svelte';
 
 const app = new App({
-  target: document.getElementById('app'),
-})
+	target: document.getElementById('app')
+});
 
-export default app
+export default app;
