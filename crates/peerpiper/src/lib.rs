0dc4f03228f462f5093b45f170aa7795647095bf
Doug Anderson444 <douganderson444@gmail.com>
2024-06-20 19:16:53 -0300

    re-export handler from peerpiper lib

--- crates/peerpiper/src/lib.rs
+++ crates/peerpiper/src/lib.rs
@@ -4,5 +4,9 @@ pub use peerpiper_browser::start;
 #[cfg(not(target_arch = "wasm32"))]
 pub use peerpiper_native::start;
 
+/// Re-export the PeerPiper handler utlitiy for using API Handlers natively.
+#[cfg(not(target_arch = "wasm32"))]
+pub use peerpiper_native::handler;
+
 /// Re-export peerpiper_core as peerpiper::core
 pub use peerpiper_core as core;
