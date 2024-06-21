a892538da3a85b4d831646c67503fbe47e5f9b77
Doug Anderson444 <douganderson444@gmail.com>
2024-06-21 14:20:04 -0300

    add Gihub pages SPA utils to index.html

--- packages/integrity-wallet/svelte.config.js
+++ packages/integrity-wallet/svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			pages: 'docs',
-			fallback: 'index.html'
+			fallback: '404.html'
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
