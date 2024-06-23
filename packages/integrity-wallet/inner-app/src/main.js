306d20b4fb08ca98da606cfac303b693c26a7bcd
Doug Anderson444 <douganderson444@gmail.com>
2024-06-23 12:36:11 -0300

    give app more unique id
    to reduce conflicts

--- packages/integrity-wallet/inner-app/src/main.js
+++ packages/integrity-wallet/inner-app/src/main.js
@@ -3,7 +3,7 @@ import '../../../../submodules/seed-keeper/crates/seed-keeper-wit-ui/output.css'
 import App from './App.svelte';
 
 const app = new App({
-	target: document.getElementById('app')
+	target: document.getElementById('integrity-wallet-app')
 });
 
 export default app;
