6cf43eb3987fddb25e106dbbf7069f2f6a9537ac
Doug Anderson444 <douganderson444@gmail.com>
2024-07-24 11:25:32 -0300

    chg to event

--- packages/bindgen-wallet/src/lib/components/AcceptOffer.svelte
+++ packages/bindgen-wallet/src/lib/components/AcceptOffer.svelte
@@ -57,7 +57,7 @@
 		<button
 			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
 			on:click={() => {
-				dispatch('accept', attributes);
+				dispatch('event', attributes);
 			}}
 		>
 			Accept Invite
