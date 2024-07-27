93bae5d65fe44dd14466625a613373afb32bae0e
Doug Anderson444 <douganderson444@gmail.com>
2024-07-27 17:56:25 -0300

    [demos]: mv Contact book to component

--- packages/demos/src/routes/app/+page.svelte
+++ packages/demos/src/routes/app/+page.svelte
@@ -1,31 +1,15 @@
 <script>
 	import { onMount, tick, createEventDispatcher } from 'svelte';
-	import { WurboComponent, wurboIn } from 'wurbo';
+	import ContactBook from './ContactBook.svelte';
 
-	// Import wasm component bytes as a url
-	import wasmURL from '../../../../../target/wasm32-wasi/release/contact_book.wasm?url';
-
-	const dispatch = createEventDispatcher();
-
-	/**
-	 * Pass in all initial content to the component
-	 * This is the initial data that the component will use to render
-	 * and interact with the user
-	 * @type {Object} data - The initial data to pass to the component
-	 */
-	export let data = {
-		tag: 'all-content',
-		val: {}
-	};
-
-	const eventHandler = (payload) => {
-		console.log('Contact Book Wasm Component emitted an event:', payload);
-		dispatch('event', payload);
-	};
+	// Handle the invite
+	function handleInvite(event) {
+		console.log('ContactBook Event:', event.detail);
+	}
 </script>
 
 <svelte:head>
 	<script src="https://cdn.tailwindcss.com"></script>
 </svelte:head>
 
-<WurboComponent {wasmURL} {data} {eventHandler} />
+<ContactBook on:event={handleInvite} />
---
+++ packages/demos/src/routes/app/ContactBook.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { onMount, tick, createEventDispatcher } from 'svelte';
+	import { WurboComponent, wurboIn } from 'wurbo';
+
+	// Import wasm component bytes as a url
+	import wasmURL from '../../../../../target/wasm32-wasi/release/contact_book.wasm?url';
+
+	const dispatch = createEventDispatcher();
+
+	/**
+	 * Pass in all initial content to the component
+	 * This is the initial data that the component will use to render
+	 * and interact with the user
+	 * @type {Object} data - The initial data to pass to the component
+	 */
+	export let data = {
+		tag: 'all-content',
+		val: {}
+	};
+
+	const eventHandler = (payload) => {
+		console.log('Contact Book Wasm Component emitted an event:', payload);
+		dispatch('event', payload);
+	};
+</script>
+
+<WurboComponent {wasmURL} {data} {eventHandler} />
