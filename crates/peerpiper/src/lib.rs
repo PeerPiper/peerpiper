7b609d6c72075a52ae7241e203778f5f28339a8b
Doug Anderson444 <douganderson444@gmail.com>
2024-10-26 12:52:46 -0300

    [peerpiper]: export NativeError as Error

--- crates/peerpiper/src/lib.rs
+++ crates/peerpiper/src/lib.rs
@@ -2,11 +2,7 @@
 pub use peerpiper_browser::start;
 
 #[cfg(not(target_arch = "wasm32"))]
-pub use peerpiper_native::start;
-
-/// Re-export the PeerPiper handler utlitiy for using API Handlers natively.
-//#[cfg(not(target_arch = "wasm32"))]
-//pub use peerpiper_native::handler;
+pub use peerpiper_native::{start, NativeError as Error};
 
 /// Re-export peerpiper_core as peerpiper::core
 pub use peerpiper_core as core;
